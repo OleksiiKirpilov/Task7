14890eec83e6b0a0ab21b7e1f64551070f4204a2
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-08-10 17:52:18 +0300

    fixes

--- src/main/java/com/epam/rd/java/basic/practice7/Util.java
+++ src/main/java/com/epam/rd/java/basic/practice7/Util.java
@@ -33,8 +33,7 @@ public class Util {
             // next line does not work and I don't know why
             sf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
             Schema schema = sf.newSchema(new File(xsd));
-            Validator validator = schema.newValidator();
-            validator.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+            Validator validator = schema.newValidator(); //NOSONAR
             validator.validate(xmlFile);
             return true;
         } catch (SAXException e) {

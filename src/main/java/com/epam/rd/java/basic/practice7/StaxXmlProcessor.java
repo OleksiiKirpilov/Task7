377883657652e7f55db7da7a8c7e0f430506a54c
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-08-10 14:08:43 +0300

    start stax

--- src/main/java/com/epam/rd/java/basic/practice7/StaxXmlProcessor.java
+++ src/main/java/com/epam/rd/java/basic/practice7/StaxXmlProcessor.java
@@ -1,4 +1,29 @@
 package com.epam.rd.java.basic.practice7;
 
+import java.util.Collections;
+
 public class StaxXmlProcessor {
+
+    private final Flowers flowers = new Flowers();
+    private final String fileName;
+
+    public void parseFile() {
+        // not implemented yet
+    }
+
+    public void saveFile(String fileName) {
+        // not implemented yet
+    }
+
+    public static void main(String[] args) {
+        StaxXmlProcessor p = new StaxXmlProcessor(args[0]);
+        p.parseFile();
+        Collections.sort(p.flowers.getFlowers());
+        p.saveFile("output.stax.xml");
+    }
+
+    public StaxXmlProcessor(String fileName) {
+        this.fileName = fileName;
+    }
+
 }
